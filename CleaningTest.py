55ed24dca6dd1722d7b20571b003d44f2f2c4095
Mambembe <matteo.giaretti@gmail.com>
2016-07-06 12:26:39 +0200

    Processes data with PySpark

--- CleaningTest.py
+++ CleaningTest.py
@@ -16,7 +16,6 @@ OUTPUT:
 
 import sys
 import os
-import pandas as pd
 
 SPARK_HOME = '/home/matteo/Spark/'
 os.environ['SPARK_HOME'] = os.path.join(SPARK_HOME)
@@ -26,57 +25,91 @@ from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 from pyspark.sql import Row
 
-
-
 # --------------------------------------------------------------------
-table_name = 'VW_STAT_Storico_Articoli'
-columnsIndex = 0#range(0,10)
-saving = True
-# --------------------------------------------------------------------
-
-
 path = '/home/matteo/Desktop/BasicNet/SanityCheck/Data/'
-sc = pyspark.SparkContext()
-sqlContext = SQLContext(sc)
-table = sc.textFile(path+table_name+'.csv').cache()
+table_name = 'dbo.shop.header'
+columnsIndex = [0,1]#range(0,10)
+saving = False
+# --------------------------------------------------------------------
 
-# Divides Header from Table
-temp = table.first()
-table = table.filter(lambda x:x != temp)
-header = temp.split(',')
+# Load SparkContext
+try:
+    sc = pyspark.SparkContext()
+    sqlContext = SQLContext(sc)
+except:
+    print ("Error loading the SparkContext")
+    sys.exit(0)
 
-def extract(line, _columnsIndex):
-    if (_columnsIndex != 0):
-        return [line[i] for i in _columnsIndex]
+# Load csv
+try:
+    table = sc.textFile(path + table_name + '.csv').cache()
+    rows = int(table.count())
+    temp = table.first()
+except:
+    print ("Errors Loading the csv file")
+    sys.exit(0)
+
+# Generates Header
+try:
+    table = table.filter(lambda x:x != temp)
+    header = temp.split(',')
+    if (columnsIndex != 0):
+        header_final = [header[i] for i in columnsIndex]
     else:
-        return [line[i] for i in len(header)]
+        columnsIndex = range(0,len(header))
+        header_final = header
+except:
+    print ("Errors while generating the header")
+    sys.exit(0)
 
-if (columnsIndex != 0):
-    header_final = [header[i] for i in columnsIndex]
-else:
-    header_final = header
+def extract(line):
+    a = True
+    return bool(a)#[line[i] for i in range(0,len(header))]
 
-data_extract = table.map(lambda line: (line.split(','))) \
-    .filter(lambda line: len(line) == len(header)) \
-    .map(extract, columnsIndex) \
-    .cache()
+def prova(line):
+    try:
+        lambda line: extract(line)
+        return True
+    except:
+        return False
 
-rows = int(table.count())
-rowsAfter = int(data_extract.count())
+#.map(extract, columnsIndex) \
 
-if saving:
-    data_extract.saveAsTextFile(path+str(table_name)+'_clean.csv')
+# Process csv
+try:
+    data_extract = table.map(lambda line: (line.split(','))) \
+        .filter(prova)\
+        .filter(lambda line: len(line) == len(header)) \
+        .cache()
+    rowsAfter = int(data_extract.count())
+except:
+    print ("Errors while processing csv")
+    sys.exit(0)
 
 
-file_name = str(table_name)+'_Results.txt'
 
-f = open(path + file_name, "w")
-f.write('----------- Test Results for ' + table_name + ' -----------\n\n')
-f.write('Columns : \n%s\n\n' %header)
-f.write('Saved columns:\n%s\n\n' %header_final)
-f.write('Number of columns before cleaning = %i\n' % len(header) + '\n')
-f.write('Number of columns after cleaning = %i\n' % len(header_final) + '\n')
-f.write('Number of rows before cleaning = %i' % rows +'\n\n')
-f.write('Number of rows after cleaning = %i' % rowsAfter +'\n\n')
-f.write('Number of deleted rows = %i' % (rows-rowsAfter))
-f.close()
+try:
+    if saving:
+        data_extract.saveAsTextFile(path+str(table_name)+'_clean.csv')
+except:
+    print ("Errors while saving the processed csv")
+    sys.exit(0)
+
+try:
+    file_name = str(table_name)+'_Results.txt'
+
+    f = open(path + file_name, "w")
+    f.write('----------- Test Results for ' + table_name + ' -----------\n\n')
+    f.write('Columns : \n%s\n\n' %header)
+    f.write('Saved columns:\n%s\n\n' %header_final)
+    f.write('Number of columns before cleaning = %i\n' % len(header) + '\n')
+    f.write('Number of columns after cleaning = %i\n' % len(header_final) + '\n')
+    f.write('Number of rows before cleaning = %i' % rows +'\n\n')
+    f.write('Number of rows after cleaning = %i' % rowsAfter +'\n\n')
+    f.write('Number of deleted rows = %i' % (rows-rowsAfter))
+    f.close()
+except:
+    print ("Errors while saving test results")
+    sys.exit(1)
+
+print "Success!"
